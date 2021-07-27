6b4bd59e651858ce37345fcbd452a04c2d943b2b
md-amir-gauhar <gauharamir15@gmail.com>
2021-07-27 23:41:54 +0530

    created home and angles component

---
+++ src/components/Hypotenuse.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../App.css'
+
+const Hypotenuse = () => {
+  return (
+    <div className="app">
+      <div className="container">
+        <Link to="/" className="back"><i class="fas fa-chevron-left"></i>Go back</Link>
+        <h1 className="title">
+          What angles (in degrees) form a triangle?
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Hypotenuse
---
+++ src/components/Quiz.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import '../App.css'
+
+const Quiz = () => {
+  return (
+    <div className="app">
+      <div className="container">
+        <Link to="/" className="back"><i class="fas fa-chevron-left"></i>Go back</Link>
+        <h1 className="title">
+          What angles (in degrees) form a triangle?
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Quiz
